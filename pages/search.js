ffc66aa0c669a66c577d8655610e9ec08813d07b
Aaron Robinsom <airobinson0025@gmail.com>
2023-02-16 15:24:12 -0500

    small adjustments

--- pages/search.js
+++ pages/search.js
@@ -8,8 +8,8 @@ import Footer from './components/Footer'
 const Search = () => {
     const router = useRouter();
     const { location, startDate, endDate, noOfGuests } = router.query;
-    const formattedStartDate = format(new Date(startDate), 'dd MMMM yy');
-    const formattedEndDate = format(new Date(endDate), 'dd MMMM yy');
+    const formattedStartDate = format(new Date(startDate), 'dd MMMM yyyy');
+    const formattedEndDate = format(new Date(endDate), 'dd MMMM yyyy');
     const range = `${formattedStartDate} - ${formattedEndDate}`;
 
     const searchResults = [
@@ -101,7 +101,7 @@ const Search = () => {
 
         <main className='flex'>
             <section className='flex-grow pt-15 px-6'>
-                <p className='text-xs'>300+ Stays - - for {noOfGuests} guests</p>
+                <p className='text-xs'>300+ Stays - {range} - for {noOfGuests} guests</p>
                 <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
                 <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
                     <p className='button'>Cancellation Flexibility</p>
